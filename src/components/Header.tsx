4e5b912419f0c6ebe2cc894dea2d8640998311b8
Tomoki Watanabe <tw20thcenturyboy@gmail.com>
2025-03-17 22:49:32 +0900

    Fix ESLint errors and cleanup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
       <div className="container mx-auto flex justify-between items-center px-4">
         {/* サイト名 */}
         <Link href="/" className="text-2xl font-bold text-blue-600">
-          Tomoki's Portfolio
+          Tomoki&apos;s Portfolio
         </Link>
 
         {/* PC向けナビゲーション */}
